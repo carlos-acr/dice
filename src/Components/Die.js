1b0175db7e5073323c2f611fe74c96afdda99b0d
Carlos Cano <carlosacr@icloud.com>
2022-06-28 22:27:48 -0500

    Added rolling animation to dice

--- src/Components/Die.js
+++ src/Components/Die.js
@@ -5,7 +5,11 @@ class Die extends Component {
   render() {
     return (
       <h1>
-        <i className={`Die fas fa-dice-${this.props.face}`} />
+        <i
+          className={`Die fas fa-dice-${this.props.face} ${
+            this.props.rolling ? "shaking" : ""
+          }`}
+        />
       </h1>
     );
   }
